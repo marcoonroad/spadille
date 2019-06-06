d28bdcf5f3fe1a5eef0fce077816b5af15cafdb0
Marco Aurélio da Silva <marcoonroad@gmail.com>
2019-06-06 20:21:09 -0300

    :construction: wip: initial steps on the generation of random numbers [+]
    also features the due setup of rollup/babel to generate modules
    backwards/retro-compatible with both old versions of Node.js and
    major browsers vendors
    
    Signed-off-by: Marco Aurélio da Silva <marcoonroad@gmail.com>

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+const commonjs = require('rollup-plugin-commonjs')
+const nodeResolve = require('rollup-plugin-node-resolve')
+const terser = require('rollup-plugin-terser').terser
+const builtins = require('rollup-plugin-node-builtins')
+const globals = require('rollup-plugin-node-globals')
+
+module.exports = {
+  input: 'src/spadille/index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'iife',
+    name: 'spadille',
+    compact: true,
+    exports: 'named',
+    globals: {
+      'crypto': 'crypto',
+    },
+  },
+  external: ['crypto'],
+  plugins: [
+    builtins({ crypto: true, }),
+    globals(),
+    nodeResolve({
+      module: true,
+      main: true,
+      browser: true
+    }),
+    commonjs({
+      exclude: ['node_modules/**'],
+      sourceMap: false
+    }),
+    terser({
+      sourcemap: false
+    })
+  ]
+}
---
+++ src/spadille/hmac/node.js
@@ -0,0 +1,16 @@
+/* eslint-env node */
+/* eslint semi: off */
+
+module.exports.init = function () {
+  const crypto = require('crypto');
+  const HASH = 'sha512';
+
+  const hmac = async function (secret, message) {
+    const hash = crypto.createHmac(HASH, secret);
+
+    hash.update(message);
+    return hash.digest('hex');
+  };
+
+  return { hmac };
+};
