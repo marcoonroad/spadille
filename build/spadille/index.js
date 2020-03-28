ef70e7cf1870680fa17bdfb862ce8e79e1fe6905
Marco Aurélio da Silva <marcoonroad@gmail.com>
2020-03-28 07:16:31 -0300

    chore: :gear: generated node and browser files with base-64 functions, still needs docs to describe feature
    Signed-off-by: Marco Aurélio da Silva <marcoonroad@gmail.com>

--- build/spadille/index.js
+++ build/spadille/index.js
@@ -1,2 +1,2 @@
 'use strict';/* eslint-env browser, env *//* eslint
-  semi: off */const lottery=require('./lottery');const prng=require('./prng');const secret=require('./secret');module.exports.lottery=lottery;module.exports.prng=prng;module.exports.secret=secret;
+  semi: off */const lottery=require('./lottery');const prng=require('./prng');const secret=require('./secret');const encoding=require('./encoding');module.exports.lottery=lottery;module.exports.prng=prng;module.exports.secret=secret;module.exports.encoding=encoding;
